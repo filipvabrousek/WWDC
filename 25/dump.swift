e51e34ea9d5c414755f116682a0b7ca130f6df99
Filip Vabrousek <filipvabrousek1@gmail.com>
2025-06-11 11:36:56 +0200

    Create dump.swift

---
+++ 25/dump.swift
@@ -0,0 +1,27 @@
+ 
+ 
+_UIHostingView<Optional<ModifiedContent<Button<Text>, PrimitiveButtonStyleContainerModifier<GlassButtonStyle>>>>:
+ superclass: UIView 
+ layer: <CALayer:0x600000c32790 
+ sublayers: <SwiftUI.SDFLayer:0x600003508790
+ 
+ 
+->_UIInheritedView:
+ superclass: _UIGraphicsView 
+ layer: <SwiftUI.SDFLayer:0x600003508790 
+ sublayers: <CALayer:0x600000c32d00 / <CALayer:0x600000c32d60
+ 
+ 
+-->_UIInheritedView:
+ superclass: _UIGraphicsView 
+ layer: <CALayer:0x600000c32d00 
+ sublayers: <CALayer:0x600000c47cf0
+ 
+ 
+--->UIPlatformGlassInteractionView:
+ superclass: UICoreGraphicsView 
+ layer: <CALayer:0x600000c47cf0 
+ sublayers: <_TtC7SwiftUIP33_863CCF9D49B535DAEB1C7D61BEE53B5914CGDrawingLayer:0x600002c21300 
+ contents:<CABackingStore 0x105b23cc0 (buffer [117 61] A8)> 
+-------------------------
+OS: iOS26
